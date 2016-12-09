c3f2662e44b8222025a360d709e9e2dcf88abd3e
Geoff <geoff@Geoffs-MacBook-Pro.local>
2016-12-09 16:20:16 -0700

    Initial commit

---
+++ src/jets/Jet.java
@@ -0,0 +1,18 @@
+package jets;
+
+public class Jet {
+
+	String model;
+	int speed;
+	int range;
+	int price;
+
+	public Jet(String model, int speed, int range, int price) {
+		super();
+		this.model = model;
+		this.speed = speed;
+		this.range = range;
+		this.price = price;
+	}
+	
+}
